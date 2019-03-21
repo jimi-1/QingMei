e3a18c29f3efc9311995fa277d1bc20f268c387e
dengxinru <1580048557@qq.com>
2019-03-21 21:31:07 +0800

    地图+人物+水果

---
+++ map1/Assets/Scripts/fruits.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class fruits : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.name == "apple")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
